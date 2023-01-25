8f6eb93130a6a803057f4197a5c8d22e5e3d0417
RizalFIrdaus <rizal.firdaus117@gmail.com>
2023-01-25 19:13:56 +0700

    HTML and PHP

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
+$title = "PHP WEB";
+$content = "WEB";
+?>
+<!DOCTYPE html>
+<html lang="en">
 
-phpinfo();
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title ?></title>
+</head>
+
+<h1><?= $content ?></h1>
+
+<body>
+
+</body>
+
+</html>
