d8e80b9902fd72f15d0b045576d4da440fcf45b9
RizalFIrdaus <rizal.firdaus117@gmail.com>
2023-01-25 20:46:01 +0700

    Header request and response

--- header.php
+++ header.php
@@ -1,10 +1,9 @@
 <?php
-$parameter = $_GET["number"];
-$result = 1;
-foreach ($parameter as $value) {
-    $result *= $value;
-}
+$client = $_SERVER["HTTP_CLIENT_NAME"];
+header("Title:BELAJAR WEB PHP");
+header("Author:M RIZAL F");
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -13,12 +12,11 @@ foreach ($parameter as $value) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Query Array Parameters</title>
+    <title>HEADER</title>
 </head>
 
 <body>
-    <h1>HAHA</h1>
-    <p>Total Perkalian : <?= $result ?></p>
+    <h1><?= $client ?></h1>
 </body>
 
 </html>
